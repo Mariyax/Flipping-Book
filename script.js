2b7f782f21810aa8d27c18a52b9757c25dff31e7
Mariya Hr. <61126935+Mariyax@users.noreply.github.com>
2025-04-09 17:11:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 
-// Event Listener
+// Event Listeners
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
@@ -23,30 +23,30 @@ function openBook() {
 }
 
 function closeBook(isAtBeginning) {
-    if(isAtBeginning) {
+    if (isAtBeginning) {
         book.style.transform = "translateX(0%)";
     } else {
         book.style.transform = "translateX(100%)";
     }
-    
+
     prevBtn.style.transform = "translateX(0px)";
     nextBtn.style.transform = "translateX(0px)";
 }
 
 function goNextPage() {
-    if(currentLocation < maxLocation) {
-        switch(currentLocation) {
+    if (currentLocation < maxLocation) {
+        switch (currentLocation) {
             case 1:
                 openBook();
-                paper1.classList.add("flipped");
+                paper1.style.display = 'block'; // Show Chapter 1
                 paper1.style.zIndex = 1;
                 break;
             case 2:
-                paper2.classList.add("flipped");
+                paper2.style.display = 'block'; // Show The End
                 paper2.style.zIndex = 2;
                 break;
             case 3:
-                paper3.classList.add("flipped");
+                paper3.style.display = 'block'; // Show Mesu AI
                 paper3.style.zIndex = 3;
                 closeBook(false);
                 break;
@@ -58,20 +58,20 @@ function goNextPage() {
 }
 
 function goPrevPage() {
-    if(currentLocation > 1) {
-        switch(currentLocation) {
+    if (currentLocation > 1) {
+        switch (currentLocation) {
             case 2:
                 closeBook(true);
-                paper1.classList.remove("flipped");
+                paper1.style.display = 'none';
                 paper1.style.zIndex = 3;
                 break;
             case 3:
-                paper2.classList.remove("flipped");
+                paper2.style.display = 'none';
                 paper2.style.zIndex = 2;
                 break;
             case 4:
                 openBook();
-                paper3.classList.remove("flipped");
+                paper3.style.display = 'none';
                 paper3.style.zIndex = 1;
                 break;
             default:
@@ -81,3 +81,6 @@ function goPrevPage() {
         currentLocation--;
     }
 }
+
+// Initial call to update pages
+goNextPage();
